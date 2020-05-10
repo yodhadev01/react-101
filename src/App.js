b6580bcce102b254452744bc1ba7482b377278b6
sagar104gurgaon <sagar104gurgaon@gmail.com>
2020-05-10 14:21:50 +0530

    2 useState obj with counter app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [firstCounter, setFirstCounter] = useState({"firstValue":0});
+  const [secondCounter, setSecondCounter] = useState({"secondValue":0});
+
+  let firstCounterUpdate = value => {
+
+    setFirstCounter({"firstValue": firstCounter.firstValue + value})
+  
+  }
+  let secondCounterUpdate = value => {
+
+    setSecondCounter({"secondValue": secondCounter.secondValue + value})
+  
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{firstCounter.firstValue}</p>
+      <p>{secondCounter.secondValue}</p>
+      <button onClick={() => firstCounterUpdate(1)}>first up</button>
+      <button onClick={() => firstCounterUpdate(-1)}>first down</button>
+      <button onClick={() => secondCounterUpdate(1)}>second up</button>
+      <button onClick={() => secondCounterUpdate(-1)}>second </button>
     </div>
   );
 }
