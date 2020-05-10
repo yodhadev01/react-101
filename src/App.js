214d94ec53c7793344c53741c2ab92f87b4607c5
sagar104gurgaon <sagar104gurgaon@gmail.com>
2020-05-10 22:09:55 +0530

    still remaining

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [active, setActive] = useState(true);
+  const [effectCount, setEffectCount] = useState(0)
+  // Similar to componentDidMount and componentDidUpdate:
+  //ComponentDidUpdate
+  useEffect(() => {
+    console.log("ComponentDidUpdate")
+    setEffectCount(effectCount + 1)
+  }, [count]);
+  //componentDidMount
+  useEffect(() => {
+    console.log("componentDidMount")
+    setEffectCount(effectCount + 100)
+  }, []);
+  useEffect(() => {
+    return() => {console.log("componentDidUnmount")}
+  }, []);  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {active ? <h1>SHOW</h1> : 
+      <div>
+      <h1>u Have Clicked it {effectCount} times</h1>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>
+        +++++
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        -----
+      </button>
+      </div>}
+      <button onClick={() => setActive(!active)}>
+        toggle
+      </button>
     </div>
-  );
+    );
 }
 
 export default App;
